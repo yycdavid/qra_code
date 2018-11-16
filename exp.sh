14b044d9a6a3e78ddb45797b876bfcb4bbec8af0
David Yang <yycdavid25@gmail.com>
2018-11-16 21:03:49 +0000

    To clean repo

--- exp.sh
+++ exp.sh
@@ -1,1 +1,3 @@
-python2 main.py --run_dir result/ --model lstm --train data/askubuntu/train --eval data/android/ --bidir --d 100 --embedding word_vectors/glove.npz --max_epoch 50 --use_content --eval_use_content --criterion cosine
+python2 main.py --run_dir result/ --cuda --model lstm --train data/askubuntu/train --eval data/android/ --bidir --d 100 --embedding word_vectors/glove.npz --max_epoch 50 --use_content --eval_use_content --criterion cosine
+
+python main_domain.py --run_dir result-adv/ --cuda --model lstm --train data/askubuntu/train --eval data/android/ --bidir --d 100 --embedding word_vectors/glove.npz --max_epoch 50 --use_content --eval_use_content --criterion cosine --cross_train data/android/ --wasserstein
